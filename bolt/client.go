a7f7d71ba0b184ec601b702980cc006ff04cf08f
Brad West <brad.g.west@gmail.com>
2019-11-18 17:49:23 -0700

    add bolt client

--- bolt/client.go
+++ bolt/client.go
@@ -3,6 +3,7 @@ package bolt
 import (
 	"time"
 
+	"github.com/boltdb/bolt"
 	"github.com/bradgwest/stott"
 )
 
@@ -19,8 +20,50 @@ type Client struct {
 	db *bolt.DB
 }
 
+// NewClient creates a new boltDB client
 func NewClient() *Client {
 	return &Client{
 		Now: time.Now,
 	}
+}
+
+// Open opens and initializes the BoltDB database.
+func (c *Client) Open() error {
+	// Open database file.
+	db, err := bolt.Open(c.Path, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	c.db = db
+
+	// Start writable transaction.
+	tx, err := c.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// Initialize top-level buckets.
+	if _, err := tx.CreateBucketIfNotExists([]byte("Statuses")); err != nil {
+		return err
+	}
+
+	// Save transaction to disk.
+	return tx.Commit()
+}
+
+// Close closes then underlying BoltDB database.
+func (c *Client) Close() error {
+	if c.db != nil {
+		return c.db.Close()
+	}
+	return nil
+}
+
+// Connect returns a new session to the BoltDB database.
+func (c *Client) Connect() *Session {
+	s := newSession(c.db)
+	s.authenticator = c.Authenticator
+	s.now = c.Now()
+	return s
 }
